8a918feb0b41ec65689a8843cb424f9b3d4ac149
daisongchen <daisongchen@www.centos.daisongchen>
2015-09-16 21:20:48 +0900

    first start

---
+++ server/templates/base_path.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+#!_*_coding:utf-8_*_
+import sys
+import os
+base_dir=os.path.dirname(os.path.abspath('.'))
+sys.path.insert(0,base_dir)
---
+++ server/templates/base_templates.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+#!_*_ coding:utf-8 _*_
+class cpu:
+	def __init__(self):
+		self.server_name='cpu_server'
+		self.params={'user':{'min':0,'max':50},
+			     'nice':{'min':0,'max':5},
+			     'system':{'min':0,'max':30},
+			     'idle':{'min':0,'max':10},
+			     'interval':3}
+class memory:
+	def __init__(self):
+		self.server_name='memory_server'
+		self.params={'mem_free':{'min':0,'max':70}
+			     ,'interval':5
+			}
+
+class hdm:
+	def __init__(self):
+		self.server_name='hdm_server'
+		self.params={'home_use':{'min':0,'max':90},
+			     'boot_use':{'min':0,'max':90},
+			     'root_use':{'min':0,'max':90},
+			     'usr_use':{'min':0,'max':90}
+             		     ,'interval':8
+		}
+
